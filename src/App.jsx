c92dac39000970fe4735b3d989d8a0165f34c192
psrth <parthsharma151@gmail.com>
2024-06-09 03:57:06 +0530

    (feat) added chakra and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Image } from "@chakra-ui/image";
+import { Flex, Text } from "@chakra-ui/layout";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Flex
+      flexDir="row"
+      height="100vh"
+      width="100vw"
+      bg="#F4F4F4"
+      padding="30px 50px"
+    >
+      <Flex flexDir="column" width="40%" height="fit-content">
+        <Image src="https://i.imgur.com/wjRdp6k.png" width="150px" />
+
+        <Flex flexDir="column">
+          <Text>Overview</Text>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
 }
 
-export default App
+export default App;
