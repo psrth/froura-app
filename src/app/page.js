433968ae364d083b062bee28f1e5b0ff29034233
psrth <parthsharma151@gmail.com>
2024-06-10 02:07:15 +0530

    (feat) changes to auth / routing

--- src/app/page.js
+++ src/app/page.js
@@ -21,8 +21,14 @@ export default function Home() {
   const [password, setPassword] = useState("");
 
   const loginHandler = () => {
+    // simulate auth for now
+    // URL parameter is unique froura ID
     if (email === "demo@ycombinator.com" && password === "Abcd1234!")
-      window.location = "/dashboard";
+      window.location = "/lm1t7WMk4jTlbFAt";
+    else if (email === "parthsharma151@gmail.com" && password === "Abcd1234!")
+      window.location = "/h3WqBKDuBAVMMYSf";
+    else if (email === "paramkapur2002@gmail.com" && password === "Abcd1234!")
+      window.location = "/Tvd5l5of3dOJHLvH";
     else {
       toast({
         title: "Login failed.",
@@ -55,26 +61,32 @@ export default function Home() {
         >
           <Flex flexDir="column">
             <Image src="https://i.imgur.com/wjRdp6k.png" width="150px" />
-            <Text mt="30px" fontSize="32px" fontWeight="600">
+            <Text mt="30px" fontSize="32px" fontWeight="600" color="#002A48">
               login
             </Text>
-            <Text mb="20px" fontSize="22px">
+            <Text mb="20px" fontSize="22px" color="#7A8F9E">
               welcome back to froura.
             </Text>
-            <Text mt="15px">email address</Text>
+            <Text mt="15px" color="#7A8F9E">
+              email address
+            </Text>
             <Input
               mt="5px"
               placeholder="john.doe@example.com"
               size="md"
               onChange={(e) => setEmail(e.target.value)}
+              color="#002A48"
             />
-            <Text mt="15px">password</Text>
+            <Text mt="15px" color="#7A8F9E">
+              password
+            </Text>
             <InputGroup mt="5px" size="md">
               <Input
                 pr="4.5rem"
                 type={show ? "text" : "password"}
                 placeholder="Enter password"
                 onChange={(e) => setPassword(e.target.value)}
+                color="#002A48"
               />
               <InputRightElement width="4.5rem">
                 <Button h="1.75rem" size="sm" onClick={handleClick}>
@@ -82,7 +94,7 @@ export default function Home() {
                 </Button>
               </InputRightElement>
             </InputGroup>
-            <Button mt="40px" onClick={loginHandler}>
+            <Button mt="40px" onClick={loginHandler} bg="#002A48" color="white">
               Login
             </Button>
           </Flex>
