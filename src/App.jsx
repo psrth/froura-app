824ab98e054fda375d06797995f03b537e141783
psrth <parthsharma151@gmail.com>
2024-06-09 13:19:51 +0530

    (feat) UI for chatbot column

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,73 @@
 import { IconButton } from "@chakra-ui/button";
 import { Image } from "@chakra-ui/image";
-import { Divider, Flex, Text } from "@chakra-ui/layout";
+import { Box, Flex, Text } from "@chakra-ui/layout";
 import { useState } from "react";
 import { FiPlus } from "react-icons/fi";
-import {
-  Table,
-  Thead,
-  Tbody,
-  Tfoot,
-  Tr,
-  Th,
-  Td,
-  TableCaption,
-  TableContainer,
-} from "@chakra-ui/table";
+import { Input } from "@chakra-ui/input";
+import { Avatar } from "@chakra-ui/avatar";
 
 function App() {
   const [show, setShow] = useState(false);
+  const [messages, setMessages] = useState([]);
+
+  const UserMessage = (props) => {
+    return (
+      <Flex flexDir="row" mt="20px">
+        <Avatar
+          src={props.img}
+          name={props.name}
+          boxSize="30px"
+          borderRadius="200px"
+          mr="20px"
+        />
+        <Flex flexDir="column">
+          <Text fontSize="16px" fontWeight="600" color="#002A48">
+            {props.name}
+          </Text>
+          <Text fontWeight="400" fontSize="18px" color="#526C7F">
+            {props.msg}
+          </Text>
+        </Flex>
+      </Flex>
+    );
+  };
+
+  const AgentMessage = (props) => {
+    return (
+      <Flex flexDir="row" mt="20px">
+        <Avatar bg="#002A48" boxSize="30px" borderRadius="200px" mr="20px" />
+        <Flex flexDir="column">
+          <Text fontSize="16px" fontWeight="600" color="#002A48">
+            {props.name}
+          </Text>
+          <Text fontWeight="400" fontSize="18px" color="#526C7F">
+            {props.msg}
+          </Text>
+          <Flex flexDir="column" p="20px">
+            {props.steps.map((st, k) => (
+              <Flex flexDir="row" m="5px 0" alignItems="center">
+                <Box
+                  bg="green"
+                  height="20px"
+                  width="20px"
+                  borderRadius="100%"
+                  mr="10px"
+                ></Box>
+                <Text color="#526C7F">{st.text}</Text>
+              </Flex>
+            ))}
+          </Flex>
+          <Text fontWeight="400" fontSize="18px" color="#526C7F">
+            {props.msg_conf}
+          </Text>
+        </Flex>
+      </Flex>
+    );
+  };
+
+  const updateMessages = (newMessages) => {
+    setMessages([...messages, ...newMessages]);
+  };
 
   return (
     <Flex
@@ -25,6 +76,7 @@ function App() {
       width="100vw"
       bg="#F4F4F4"
       padding="30px 50px"
+      justifyContent="space-between"
     >
       {/* LEFT COLUMN */}
       <Flex flexDir="column" width="40%" height="fit-content">
@@ -103,19 +155,19 @@ function App() {
               <Flex
                 width="100%"
                 height="1px"
-                bg="black"
+                bg="#526C7F"
                 opacity="0.2"
                 m="10px 0"
               />
               <Flex flexDir="row" width="100%" justifyContent="space-between">
-                <Text fontSize="18px" fontWeight="500">
+                <Text fontSize="16px" fontWeight="400">
                   {tr.title}
                 </Text>
-                <Text fontSize="18px" fontWeight="800">
+                <Text fontSize="16px" fontWeight="600">
                   ${tr.amount}
                 </Text>
               </Flex>
-              <Text color="#5D5D5D">
+              <Text color="#5D5D5D" fontSize="14px">
                 {tr.payee}, {tr.time}
               </Text>
             </Flex>
@@ -124,6 +176,96 @@ function App() {
         </Flex>
       </Flex>
       {/* END LEFT COLUMN */}
+
+      {/* RIGHT COLUMN */}
+      <Flex
+        flexDir="column"
+        width="55%"
+        bg="white"
+        height="90vh"
+        borderRadius="20px"
+        boxShadow="lg"
+        padding="40px 80px"
+        justifyContent="space-between"
+      >
+        <Flex flexDir="column">
+          <Text fontSize="20px" fontWeight="400" color="#526C7F">
+            froura assistant
+          </Text>
+          <Flex
+            width="100%"
+            height="1px"
+            bg="#526C7F"
+            opacity="0.2"
+            m="10px 0"
+          />
+          <Flex
+            flexDir="column"
+            height="fit-content"
+            maxHeight="65vh"
+            overflow="scroll"
+          >
+            {messages.map((m, e) => (
+              <>
+                {m.type === "user" ? (
+                  <UserMessage img={m.img} name={m.name} msg={m.msg} />
+                ) : null}
+                {m.type === "agent" ? (
+                  <AgentMessage
+                    name={m.name}
+                    msg={m.msg}
+                    steps={m.steps}
+                    msg_conf={m.msg_conf}
+                  />
+                ) : null}
+              </>
+            ))}
+          </Flex>
+        </Flex>
+        <Input
+          padding="10px 20px"
+          border="1px solid"
+          borderColor="#CBD3D9"
+          variant="outline"
+          borderRadius="30px"
+          placeholder="How can I help?"
+          onClick={() =>
+            updateMessages([
+              {
+                type: "user",
+                name: "Parth Sharma",
+                img: "https://avatars.githubusercontent.com/u/45586386?v=4",
+                msg: "Can you order me a hamburger from McDonald’s?",
+              },
+              {
+                type: "agent",
+                name: "froura.ai",
+                msg: "Sure! Just give me a second to process that request. I’ll connect with Doordash and send you a request to approve the final payment.",
+                steps: [
+                  {
+                    text: "Connecting to DoorDash",
+                    status: "Completed",
+                  },
+                  {
+                    text: "Sending DoorDash order and customer details",
+                    status: "Progress",
+                  },
+                  {
+                    text: "Raising a payment request",
+                    status: "Pending",
+                  },
+                  {
+                    text: "Waiting for confirmation of payment receipt",
+                    status: "Pending",
+                  },
+                ],
+                msg_conf:
+                  "Done! Your order is placed. It should arrive within 30 minutes.",
+              },
+            ])
+          }
+        />
+      </Flex>
     </Flex>
   );
 }
